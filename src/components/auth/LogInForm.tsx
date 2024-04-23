0100c7c4ea74bd5f7cf440dd24aa3b583f95a16c
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-04-23 10:38:07 +0200

    create password field and init onSubmit

--- src/components/auth/LogInForm.tsx
+++ src/components/auth/LogInForm.tsx
@@ -4,9 +4,10 @@ import CardWrapper from './CardWrapper'
 import { useForm } from 'react-hook-form'
 import { LoginSchema } from '../../../schemas'
 import * as z from "zod"
-import  {zodResolver}  from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
 import { Input } from '../ui/input'
+import { Button } from '../ui/button'
 const LogInForm = () => {
     // states 
 
@@ -14,54 +15,92 @@ const LogInForm = () => {
     const form = useForm<z.infer<typeof LoginSchema>>(
         {
             resolver: zodResolver(LoginSchema),
-            defaultValues:{
+            defaultValues: {
                 email: "",
-                password:""
+                password: ""
             }
         }
 
     )
-    return (
-        <CardWrapper
+
+    // create a function to submit the form
+    const onSubmit = (values:z.infer<typeof LoginSchema>) => 
+        { console.log(values) }
+return (
+    <CardWrapper
         headerLabel="Welcome Back"
-        backButtonLabel= "Don't have an account?"
+        backButtonLabel="Don't have an account?"
         backButtonHref="/auth/register"
         showSocial
-        >
-            <Form {...form}>
+    >
+        <Form {...form}>
 
-                <form onSubmit={form.handleSubmit(()=>{})}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className=' flex flex-col gap-3'>
+                <div className="">
                     {/* email item */}
                     <FormField
-                    control={form.control}
-                    name="email"
-                    render={
-                        ({field})=>(
-                            <FormItem>
-                                
-                                {/* {console.log(field)} */}
+                        // control={form.control}
+                        name="email"
+                        render={
+                            ({ field }) => (
+                                <FormItem>
 
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input 
-                                    {...field}
-                                    type='email'
-                                    placeholder='Enter Your Email'
-                                    />
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )
-                    }
+                                    {/* {console.log(field)} */}
+
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            type='email'
+                                            placeholder='Enter Your Email'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )
+                        }
                     >
-                        
+
 
                     </FormField>
-                </form>
+                    {/* password item */}
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={
+                            ({ field }) => (
+                                <FormItem>
 
-            </Form>
-        </CardWrapper>
-    )
+                                    {/* {console.log(field)} */}
+
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            type='password'
+                                            placeholder='Enter Your Password'
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )
+                        }
+                    >
+
+
+                    </FormField>
+
+                </div>
+                <Button type="submit" className='mt-2 w-full' size={'lg'}>
+                    Login
+                </Button>
+
+
+            </form>
+
+        </Form>
+    </CardWrapper>
+)
 }
 
 export default LogInForm
